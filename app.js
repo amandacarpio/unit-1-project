dd562ebf4188f3d0cedb301dd8744d22b6ba8021
Amanda Carpio <amandamarielc@gmail.com>
2022-10-24 23:49:31 -0400

    commits for 12/24 - not done yet

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
-const apiKey = ''
-const baseURL = ''
+const key = 'AIzaSyBjROxJW481jiKnusSJF4g1Lx2b61cY9hQ'
+const baseURL = 'https://www.googleapis.com/books/v1/volumes?'
+let bookData, firstLast
+const $input = $("input[type=text]") // placed text input in a variable to shorten it/easier to read
+
+$("form").on("submit", authorSearch)
 
 // function that does thriller book search
+function authorSearch(){
+    const url = `${baseURL}q=${firstLast}+subject:thriller&key=${key}`
+    firstLast = $input.val()
+    $.ajax(url)
+    .then((books) => {
+        console.log(bookData)
+        bookData = books
+    })
+//     function render(){
+
+//     }
+}
+
+
+
 // constructing url for request
 // make our request
 // render the data
